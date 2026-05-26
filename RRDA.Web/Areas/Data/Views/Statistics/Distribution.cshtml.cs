82807be770a1a756b197112e25444478d2664eab
Michele Santucci <m.santucci@idscorporation.com>
2026-05-26 13:30:33 +0200

    Prima demo visualizzazione statistica

---
+++ RRDA.Web/Areas/Data/Views/Statistics/Distribution.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RRDA.Web.Areas.Data.Views.Statistics
+{
+    public class DistributionModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
